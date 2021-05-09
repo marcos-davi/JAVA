82212dffdfe6fc8702559d32848869200b763c2c
marcos-davi <77940812+marcos-davi@users.noreply.github.com>
2021-05-10 00:54:32 +0200

    Create FibonacciIterativo.java

---
+++ FibonacciIterativo.java
@@ -0,0 +1,27 @@
+package TESTES.pruebas;
+
+public class FibonacciIterativo {
+
+    public static void main(String[] args) {
+
+        int  a = 0, b = 1, c = 0, i;
+
+        // Muestro el valor inicial
+
+
+        for ( i = 0; i < 10; i++) {
+            if (i < 9) {
+                System.out.print(a + ", ");
+                //primero sumamos
+                c = a + b;
+                //Despues, cambiamos la segunda variable por la primera
+                a = b;
+                //Por ultimo, cambiamos la suma por la segunda variable
+                b = c;
+            }else
+                System.out.println(a);
+        }
+    }
+}
+
+
